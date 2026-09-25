0e27f918ffa957601adf77fe0f7a4e29fa137437
sreebhargavi872-prog <sreebhargavi872@gmail.com>
2026-09-25 09:58:31 +0530

    Create variables.cpp

---
+++ variables.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 20;
+    float marks = 85.5;
+    char grade = 'A';
+
+    cout << "Age: " << age << endl;
+    cout << "Marks: " << marks << endl;
+    cout << "Grade: " << grade << endl;
+
+    return 0;
+}
