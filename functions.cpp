f1423e408bb74645fa16c80fa0e6f5f744a17e18
sreebhargavi872-prog <sreebhargavi872@gmail.com>
2026-09-25 11:14:43 +0530

    Create functions.cpp

---
+++ functions.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int result = add(10, 20);
+
+    cout << "Sum = " << result;
+
+    return 0;
+}
