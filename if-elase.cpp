bd40626c5870a9874df615c8f2fa36a080a0fe9a
sreebhargavi872-prog <sreebhargavi872@gmail.com>
2026-09-25 11:06:19 +0530

    Create if-elase.cpp

---
+++ if-elase.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks;
+
+    cout << "Enter your marks: ";
+    cin >> marks;
+
+    if (marks >= 40) {
+        cout << "Pass";
+    } else {
+        cout << "Fail";
+    }
+
+    return 0;
+}
